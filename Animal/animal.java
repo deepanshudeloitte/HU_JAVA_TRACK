058487a814de53eea1eb73811abc8e804afa365f
deepanshudeloitte <deepanshusingh8@deloitte.com>
2022-03-16 15:14:31 +0530

    Update animal.java
    adding new comments in code to understand

--- Animal/animal.java
+++ Animal/animal.java
@@ -1,15 +1,16 @@
 public class animal {
     public static void main(String[] args) {
+        //Two types of monkey defined by name and color monkey1 and monkey2 respectively
      monkey1 m = new monkey1();
-        monkey2 n = new monkey2();
+     monkey2 n = new monkey2();
         m.setArms(2);
         m.setColor("grey");
         m.setName("Langur monkey");
         m.display();
-        n.setName("Macaque Monkey");
-        n.setColor("brown");
-        n.setArms(2);
-        n.display();
+            n.setName("Macaque Monkey");
+            n.setColor("brown");
+            n.setArms(2);
+            n.display();
 
 
     }
