5315cb225368fbdcd06df5e32e4a3833c8b3e689
deepanshudeloitte <deepanshusingh8@deloitte.com>
2022-03-17 17:26:11 +0530

    changes done in the file

--- reverse_of_a_list/reverseString.java
+++ reverse_of_a_list/reverseString.java
@@ -12,8 +12,5 @@ public class reverseString {
         String []a = {"hashedin", "by", "deloitte","2022"};
         reverse(a);
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> 555b9c68eb7ab3e5e73b8a13c6676a7e02c64d4b
 }
